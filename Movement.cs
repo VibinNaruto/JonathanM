4d269e24e3383c666e06e1719d6d8c804fbf78c1
Jonathan Mendoza <jonathan.mendozzaa@gmail.com>
2023-08-30 15:36:52 -0600

    Movement and Camera for player
    These are simple yet effective camera follow and movement for player. Gonna work on some of the stuff we did in class

---
+++ Movement.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+
+public float speed;
+private float Move;
+
+private Rigidbody2D rb;
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move = Input.GetAxis("Horizontal");
+
+        rb.velocity = new Vector2(speed * Move, rb.velocity.y);
+
+    }
+}
