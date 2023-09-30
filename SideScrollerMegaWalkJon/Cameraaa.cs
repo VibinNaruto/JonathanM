9732219d530294063cf85a7d0afcef79e2756126
Jonathan Mendoza <jonathan.mendozzaa@gmail.com>
2023-09-29 19:28:01 -0600

    Some Clean up

--- SideScrollerMegaWalkJon/Cameraaa.cs
+++ SideScrollerMegaWalkJon/Cameraaa.cs
@@ -6,11 +6,6 @@ public class Cameraaa : MonoBehaviour
 {
    
     public GameObject target;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
     // Update is called once per frame
     void Update()
