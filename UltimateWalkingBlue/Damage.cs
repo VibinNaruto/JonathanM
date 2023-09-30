b70eba7a17b6faf86ec04b18fcf5b2a310f54aea
Jonathan Mendoza <jonathan.mendozzaa@gmail.com>
2023-09-29 19:35:58 -0600

    Had to make a new folder but this is a cleaned up version of my code

--- UltimateWalkingBlue/Damage.cs
+++ UltimateWalkingBlue/Damage.cs
@@ -6,17 +6,6 @@ public class Damage : MonoBehaviour
 {
     public playerHealth pHealth;
     public float damage;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnCollisionEnter(Collision other)
     {
--- UltimateWalkingBlue/GameManagerCode.cs
+++ UltimateWalkingBlue/GameManagerCode.cs
@@ -7,18 +7,6 @@ public class GameManagerCode : MonoBehaviour
 {
     public GameObject gameOverUI;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
     public void gameOver()
     {
         gameOverUI.SetActive(true);
