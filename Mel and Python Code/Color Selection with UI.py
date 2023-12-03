f4364e05a2901ac9f2345715561c51bbde82d525
Jonathan Mendoza <jonathan.mendozzaa@gmail.com>
2023-12-03 10:46:58 -0700

    These are my final Scripts with UI implemented in them.
    THEY WORK!!!

---
+++ Mel and Python Code/Color Selection with UI.py
@@ -0,0 +1,76 @@
+import maya.cmds as cmds
+
+def change_override_color(selected_objects, color_index):
+    if not selected_objects:
+        cmds.warning("No objects selected. Please select some objects.")
+        return
+
+    for obj in selected_objects:
+        shapes = cmds.listRelatives(obj, shapes=True, fullPath=True) or []
+        for shape in shapes:
+            cmds.setAttr(shape + ".overrideColor", color_index)
+            cmds.setAttr(f"{shape}.overrideEnabled", 1)
+
+class ColorSelectionUI():
+    ui_name = 'Color'
+    window_name = '%sWindow' % ui_name
+
+    def __init__(self):
+        self.num_field_list = None
+        self.operation_type = None
+        self.num_values = None
+        self.m_col = None
+        self.color_slider = None
+
+    def delete(self):
+        if cmds.window(ColorSelectionUI.window_name, exists=True):
+            cmds.deleteUI(ColorSelectionUI.window_name)
+
+    def create(self):
+        self.delete()
+        self.window_name = cmds.window(ColorSelectionUI.window_name,
+                                       title='%s Tool' % (ColorSelectionUI.ui_name),
+                                       widthHeight=(504, 139),
+                                       sizeable=False)
+
+        self.m_col = cmds.columnLayout(p=ColorSelectionUI.window_name, adj=True)
+
+        cmds.separator(p=self.m_col, style='in', h=10)
+
+        self.color_slider = cmds.intSliderGrp(p=self.m_col,
+                                              label='Select Color',
+                                              field=True,
+                                              minValue=1,
+                                              maxValue=31,
+                                              value=1,
+                                              cw=[(1, 150), (2, 50)],
+                                              cl3=['center', 'center', 'center'],
+                                              dragCommand=lambda *args: self.update_color())
+
+        cmds.separator(p=self.m_col, style='in', h=10)
+
+        cmds.button(p=self.m_col,
+                    l='Change Color!',
+                    command=lambda *args: self.calculate_and_change_color())
+
+        cmds.separator(p=self.m_col, style='in', h=10)
+
+        self.show()
+
+    def update_num_fields(self):
+        pass
+
+    def update_color(self):
+        pass
+
+    def calculate_and_change_color(self):
+        selected_objects = cmds.ls(selection=True, long=True)
+        color_index = cmds.intSliderGrp(self.color_slider, q=True, value=True)  # Get color from the slider
+        change_override_color(selected_objects, color_index)
+
+    def show(self):
+        cmds.showWindow(ColorSelectionUI.window_name)
+
+# Example usage:
+colorSelection_ui = ColorSelectionUI()
+colorSelection_ui.create()
---
+++ Mel and Python Code/RandomGeneratorWithUI.py
@@ -0,0 +1,67 @@
+import maya.cmds as cmds
+import random
+
+def placement_generator(x_min, x_max, y_min, y_max, z_min, z_max, num_dups):
+    sels = cmds.ls(selection=True)
+
+    if not sels:
+        print("No objects selected.")
+        return
+
+    for sel in sels:
+        for _ in range(num_dups):
+            dup = cmds.duplicate(sel, rr=True)[0]
+
+            rand_x = random.uniform(x_min, x_max)
+            rand_y = random.uniform(y_min, y_max)
+            rand_z = random.uniform(z_min, z_max)
+
+            cmds.setAttr(f"{dup}.translateX", rand_x)
+            cmds.setAttr(f"{dup}.translateY", rand_y)
+            cmds.setAttr(f"{dup}.translateZ", rand_z)
+
+class RandomGeneratorUI():
+    ui_name = 'RandomGen'
+    window_name = '%sWindow' % ui_name
+
+    def __init__(self):
+        self.num_field_list = None
+        self.operation_type = None
+        self.num_values = None
+        self.m_col = None
+
+    def delete(self):
+        if cmds.window(RandomGeneratorUI.window_name, exists=True):
+            cmds.deleteUI(RandomGeneratorUI.window_name)
+
+    def create(self):
+        self.delete()
+        self.window_name = cmds.window(RandomGeneratorUI.window_name,
+                                       title='%s Tool' % (RandomGeneratorUI.ui_name),
+                                       widthHeight=(504, 139),
+                                       sizeable=False)
+
+        self.m_col = cmds.columnLayout(p=RandomGeneratorUI.window_name, adj=True)
+
+        cmds.separator(p=self.m_col, style='in', h=10)
+
+        cmds.button(p=self.m_col,
+                    l='Randomize!',
+                    command=lambda *args: self.update_num_fields())
+
+        cmds.separator(p=self.m_col, style='in', h=10)
+
+        self.show()
+
+    def update_num_fields(self):
+        # Call the placement_generator function with the desired arguments
+        placement_generator(-10, 30, -5, 20, -100, 100, 50)
+
+        # Optionally, you can add code here to update the UI based on the calculation
+
+    def show(self):
+        cmds.showWindow(RandomGeneratorUI.window_name)
+
+# Create and show the UI
+myTest = RandomGeneratorUI()
+myTest.create()
